55b9b4feafe65d458ad2149d786405d1f612327b
Khoa Do <khoado.dk@gmail.com>
2021-02-08 20:17:30 -0500

    Added Functionality

---
+++ assets/application.js
@@ -0,0 +1,265 @@
+$(document).ready(function () {
+  // Global variables
+  let moneySpanSelector = "span.money",
+    currencyPickerSelector = "[name=currencies]",
+    activeCurrencySelector = ".js-active-currency",
+    currencyNoteSelector = ".js-cart-currency-note";
+
+  let currencyPicker = {
+    loadCurrency: function () {
+      /* Fix for customer account pages */
+      $(moneySpanSelector + " " + moneySpanSelector).each(function () {
+        $(this).parents(moneySpanSelector).removeClass("money");
+      });
+
+      /* Saving the current price */
+      $(moneySpanSelector).each(function () {
+        $(this).attr("data-currency-" + shopCurrency, $(this).html());
+      });
+
+      // If there's no cookie.
+      if (cookieCurrency == null) {
+        if (shopCurrency !== defaultCurrency) {
+          Currency.convertAll(shopCurrency, defaultCurrency);
+        } else {
+          Currency.currentCurrency = defaultCurrency;
+        }
+      }
+      // If the cookie value does not correspond to any value in the currency dropdown.
+      else if (
+        $(currencyPickerSelector).length &&
+        $(currencyPickerSelector + "option[value=" + cookieCurrency + "]")
+          .length === 0
+      ) {
+        Currency.currentCurrency = shopCurrency;
+        Currency.cookie.write(shopCurrency);
+      } else if (cookieCurrency === shopCurrency) {
+        Currency.currentCurrency = shopCurrency;
+      } else {
+        $(currencyPickerSelector).val(cookieCurrency);
+        Currency.convertAll(shopCurrency, cookieCurrency);
+      }
+
+      currencyPicker.setCurrencyText();
+    },
+    onCurrencyChanged: function (event) {
+      let newCurrency = $(this).val(),
+        $otherpickers = $(currencyPickerSelector).not(this);
+
+      Currency.convertAll(Currency.currentCurrency, newCurrency);
+      currencyPicker.setCurrencyText(newCurrency);
+
+      if ($otherpickers.length > 0) {
+        $otherpickers.val(newCurrency);
+      }
+    },
+
+    setCurrencyText: function (newCurrency = Currency.currentCurrency) {
+      let $activeCurrency = $(activeCurrencySelector),
+        $currencyNote = $(currencyNoteSelector);
+
+      if ($activeCurrency.length > 0) {
+        $activeCurrency.text(newCurrency);
+      }
+
+      if ($currencyNote.length > 0) {
+        if (newCurrency !== shopCurrency) {
+          $currencyNote.show();
+        } else {
+          $currencyNote.hide();
+        }
+      }
+    },
+
+    onMoneySpanAdded: function () {
+      Currency.convertAll(shopCurrency, Currency.currentCurrency);
+      currencyPicker.setCurrencyText();
+    },
+
+    init: function () {
+      if (showMultipleCurrencies != true) {
+        return false;
+      }
+
+      currencyPicker.loadCurrency();
+
+      $(document).on(
+        "change",
+        currencyPickerSelector,
+        currencyPicker.onCurrencyChanged
+      );
+    },
+  };
+
+  currencyPicker.init();
+
+  // ADD TO CART FORM
+  let addToCartFormSelector = "#add-to-cart-form",
+    productOptionSelector = addToCartFormSelector + " [name*=option]";
+
+  let productForm = {
+    onProductOptionChanged: function (event) {
+      let $form = $(this).closest(addToCartFormSelector),
+        selectedVariant = productForm.getActiveVariant($form);
+      // Pass selected variant variable when form change
+      $form.trigger("form:change", [selectedVariant]);
+    },
+    getActiveVariant: function ($form) {
+      let variants = JSON.parse(
+          decodeURIComponent($form.attr("data-variants"))
+        ),
+        formData = $form.serializeArray();
+      // Shopify has variant.option1, variant.option2, variant.option3  by default
+      formOptions = {
+        option1: null,
+        option2: null,
+        option3: null,
+      };
+      let selectedVariant = null;
+      $.each(formData, function (index, item) {
+        if (item.name.indexOf("option") !== -1) {
+          formOptions[item.name] = item.value;
+        }
+      });
+      // Return selectedVariant only all 3 options selected
+      $.each(variants, function (index, variant) {
+        if (
+          variant.option1 === formOptions.option1 &&
+          variant.option2 === formOptions.option2 &&
+          variant.option3 === formOptions.option3
+        ) {
+          selectedVariant = variant;
+          return false;
+        }
+      });
+      return selectedVariant;
+
+      // console.log("variants", variants);
+      // console.log("FormData", formData);
+      // console.log("Form Options", formOptions);
+    },
+    validate: function (event, selectedVariant) {
+      // Check if selectedVariant and send variant product id to cart, disable addtocart button
+      let $form = $(this),
+        hasVariant = selectedVariant !== null,
+        canAddToCart = hasVariant && selectedVariant.inventory_quantity > 0,
+        $id = $form.find(".js-variant-id"),
+        $addToCartButton = $form.find("#add-to-cart-button"),
+        // Change price based on selectedVariant
+        $price = $form.find(".js-price"),
+        formattedVariantPrice,
+        priceHtml;
+
+      if (hasVariant) {
+        formattedVariantPrice = "$" + (selectedVariant.price / 100).toFixed(2);
+        priceHtml = '<span class="money">' + formattedVariantPrice + "</span>";
+        window.history.replaceState(
+          null,
+          null,
+          "?variant=" + selectedVariant.id
+        );
+      } else {
+        priceHtml = $price.attr("data-default-price");
+      }
+
+      if (canAddToCart) {
+        $id.val(selectedVariant.id);
+        $addToCartButton.prop("disabled", false);
+      } else {
+        $id.val("");
+        $addToCartButton.prop("disabled", true);
+      }
+
+      // Add money span to html and convert the currency
+      $price.html(priceHtml);
+      currencyPicker.onMoneySpanAdded();
+    },
+    init: function () {
+      $(document).on(
+        "change",
+        productOptionSelector,
+        productForm.onProductOptionChanged
+      );
+      $(document).on(
+        "form:change",
+        addToCartFormSelector,
+        productForm.validate
+      );
+    },
+  };
+
+  productForm.init();
+
+  // AJAX API
+  let ajaxify = {
+    onAddToCart: function (event) {
+      event.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "/cart/add.js",
+        data: $(this).serialize(),
+        dataType: "json",
+        success: ajaxify.onCartUpdated,
+        error: ajaxify.onError,
+      });
+    },
+    onLineRemoved: function (event) {
+      event.preventDefault();
+      let $removeLink = $(this),
+        removeQuery = $removeLink.attr("href").split("change?")[1];
+      $.post("/cart/change.js", removeQuery, ajaxify.onCartUpdated, "json");
+    },
+    onCartUpdated: function () {
+      $.ajax({
+        type: "GET",
+        url: "/cart",
+        content: document.body,
+        success: function (context) {
+          let $dataCartContents = $(context).find(".js-cart-page-contents"),
+            dataCartHtml = $dataCartContents.html(),
+            dataCartItemCount = $dataCartContents.attr("data-cart-item-count"),
+            $miniCartContents = $(".js-mini-cart-contents"),
+            $cartItemCount = $(".js-cart-item-count");
+
+          $cartItemCount.text(dataCartItemCount);
+          $miniCartContents.html(dataCartHtml);
+          if (parseInt(dataCartItemCount) > 0) {
+            ajaxify.openCart();
+          } else {
+            ajaxify.closeCart();
+          }
+        },
+      });
+    },
+    onError: function (XMLHttpRequest, textStatus) {
+      let data = XMLHttpRequest.responseJSON;
+      alert(data.status + " - " + data.message + ": " + data.description);
+    },
+    onCartButtonClick: function (event) {
+      event.preventDefault();
+      let isCartOpen = $("html").hasClass("mini-cart-open");
+      if (!isCartOpen) {
+        ajaxify.openCart();
+      } else {
+        ajaxify.closeCart();
+      }
+    },
+    openCart: function () {
+      $("html").addClass("mini-cart-open");
+    },
+    closeCart: function () {
+      $("html").removeClass("mini-cart-open");
+    },
+    init: function () {
+      $(document).on("submit", addToCartFormSelector, ajaxify.onAddToCart);
+      $(document).on(
+        "click",
+        "#mini-cart .js-remove-line",
+        ajaxify.onLineRemoved
+      );
+      $(document).on("click", ".js-cart-link", ajaxify.onCartButtonClick);
+    },
+  };
+
+  ajaxify.init();
+});
